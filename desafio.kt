07a5cd159c025fc276f70c2ffc38c301850ed789
Vinícius Vilas Boas <vinivilasboas@gmail.com>
2023-01-11 19:24:15 -0300

    Atualização do desafio

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,71 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario (val id: Int, val nome: String, val login: String) {
+
+    var score = 0 // score vai aumentando conforme os cursos são concluídos
+
+    override fun toString() = "NOME = " + nome + "||| SCORE = " + score 
+
+}
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos = mutableListOf<Usuario>() // lista de inscritos na formação
+
+    override fun toString(): String {
+        return "********************" + 
+        "\nNOME = " + nome +
+        "\nConteúdo = " + conteudos +
+        "\nMatriculados = " + inscritos +
+        "\n********************"
+    }
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuario: Usuario) {
+        inscritos.addAll(usuario)
+    }
+
+    fun concluirConteudoEducacional(usuario: Usuario) {
+        usuario.score += 60
+    }
+
+    fun concluirFormacao (usuario: Usuario) {
+        usuario.score += 150
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val dinoDaSilvaSauro = Usuario(1, "Dino da Silva Sauro", "dinosauro")
+    val franDaSilvaSauro = Usuario(2, "Fran da Silva Sauro", "fransauro")
+    val robbieSinclair = Usuario(3, "Robbie Sinclair", "robbiesinclair")
+    val charleneDaSilvaSauro = Usuario(4, "Charlene da Silva Sauro", "charlenesauro")
+    val babySinclair = Usuario(5, "Baby Sinclair", "babysinclair")
+
+    val kotlinBasico = ConteudoEducacional("Kotlin Basico")
+    val classes = ConteudoEducacional("Classes")
+    val collections = ConteudoEducacional("Collections")
+
+    val cursosKotlin = mutableListOf(kotlinBasico, classes, collections)
+
+    val kotlinParaDinos = Formacao("Kotlin Para Dinos", cursosKotlin, Nivel.BASICO)
+
+    kotlinParaDinos.matricular(dinoDaSilvaSauro, 
+                                franDaSilvaSauro, 
+                                robbieSinclair, 
+                                charleneDaSilvaSauro, 
+                                babySinclair)
+
+    kotlinParaDinos.concluirConteudoEducacional(dinoDaSilvaSauro)
+    kotlinParaDinos.concluirFormacao(dinoDaSilvaSauro)
+
+    println(dinoDaSilvaSauro.toString())
+    println(franDaSilvaSauro.toString())
+
+    println(kotlinBasico.toString())
+
+    println(kotlinParaDinos.toString())
+
 }
